d886dbe4df36c4183ec34f9af17da2ce5e4ed6ec
Shirley Cohen <scohen@cs.utexas.edu>
2018-11-30 16:33:33 -0600

    Add files via upload

--- bureau_labor_stats_ctas.sql
+++ bureau_labor_stats_ctas.sql
@@ -1,8 +1,8 @@
 create table bureau_labor_stats.All_Industries_Wages
 (
 	area INT64,
-	soc_code STRING,
 	year INT64,
+	soc_code STRING,
 	hourly_wage FLOAT64,
 	annual_salary FLOAT64,
 	empty_date DATE
@@ -10,48 +10,48 @@ create table bureau_labor_stats.All_Industries_Wages
 PARTITION BY empty_date
 CLUSTER BY year;
 
-insert into bureau_labor_stats.All_Industries_Wages (area, soc_code, year, hourly_wage, annual_salary) 
-select Area, SocCode, 2015, Average, ((Average*8)*365) 
+insert into bureau_labor_stats.All_Industries_Wages (area, year, soc_code, hourly_wage, annual_salary) 
+select Area, 2015, SocCode, Average, ((Average*8)*365) 
 from bureau_labor_stats.All_Industries_Wages_2015;
 	
-insert into bureau_labor_stats.All_Industries_Wages (area, soc_code, year, hourly_wage, annual_salary) 
-select Area, SocCode, 2016, Average, ((Average*8)*365) 
+insert into bureau_labor_stats.All_Industries_Wages (area, year, soc_code, hourly_wage, annual_salary) 
+select Area, 2016, SocCode, Average, ((Average*8)*365) 
 from bureau_labor_stats.All_Industries_Wages_2016;
 
-insert into bureau_labor_stats.All_Industries_Wages (area, soc_code, year, hourly_wage, annual_salary) 
-select Area, SocCode, 2017, Average, ((Average*8)*365) 
+insert into bureau_labor_stats.All_Industries_Wages (area, year, soc_code, hourly_wage, annual_salary) 
+select Area, 2017, SocCode, Average, ((Average*8)*365) 
 from bureau_labor_stats.All_Industries_Wages_2017;
 
-insert into bureau_labor_stats.All_Industries_Wages (area, soc_code, year, hourly_wage, annual_salary) 
-select Area, SocCode, 2018, Average, ((Average*8)*365) 
+insert into bureau_labor_stats.All_Industries_Wages (area, year, soc_code, hourly_wage, annual_salary) 
+select Area, 2018, SocCode, Average, ((Average*8)*365) 
 from bureau_labor_stats.All_Industries_Wages_2018;
 	
 
 create table bureau_labor_stats.Geography
 (
 	area INT64,
+	year INT64,
 	state STRING,
 	county STRING,
-	year INT64,
 	empty_date DATE
 )
 PARTITION BY empty_date
 CLUSTER BY year;
 	
-insert into bureau_labor_stats.Geography (area, state, county, year) 
-select Area, StateAb, CountyTownName, 2015
+insert into bureau_labor_stats.Geography (area, year, state, county) 
+select Area, 2015, StateAb, CountyTownName
 from bureau_labor_stats.Geography_2015;
 
-insert into bureau_labor_stats.Geography (area, state, county, year) 
-select Area, StateAb, CountyTownName, 2016
+insert into bureau_labor_stats.Geography (area, year, state, county) 
+select Area, 2016, StateAb, CountyTownName
 from bureau_labor_stats.Geography_2016;
 
-insert into bureau_labor_stats.Geography (area, state, county, year) 
-select Area, StateAb, CountyTownName, 2017
+insert into bureau_labor_stats.Geography (area, year, state, county) 
+select Area, 2017, StateAb, CountyTownName
 from bureau_labor_stats.Geography_2017;
 
-insert into bureau_labor_stats.Geography (area, state, county, year) 
-select Area, StateAb, CountyTownName, 2018
+insert into bureau_labor_stats.Geography (area, year, state, county) 
+select Area, 2018, StateAb, CountyTownName
 from bureau_labor_stats.Geography_2018;
 
 
--- transform_job_table_single.py
+++ transform_job_table_single.py
@@ -87,74 +87,107 @@ class MakeBigQueryRecord(beam.DoFn):
         job_results = []
         for job_record in job_matches:
             
-            # check to see if worksite_city, worksite_county, worksite_state, worksite_postal_code are valid
-            valid_worksite = 1
-            if job_record['worksite_city'] == None: 
-                valid_worksite = 0
-                job_record.pop('worksite_city')
-            if job_record['worksite_county'] == None:
-                valid_worksite = 0
-                job_record.pop('worksite_county')
-            if job_record['worksite_state'] == None:
-                valid_worksite = 0
-                job_record.pop('worksite_state')
-            if job_record['worksite_postal_code'] == None:
-                valid_worksite = 0
-                job_record.pop('worksite_postal_code')
-            if valid_worksite == 1:
-                # put them into the appropriate columns if needed
-                worksite_postal_code_start = job_record['worksite_postal_code'][0]
+            # validate worksite_city, worksite_county, worksite_state, worksite_postal_code
+            non_empty_worksite = 1
+            worksite_city = None
+            worksite_county = None
+            worksite_state = job_record['worksite_state']
+            worksite_postal_code = job_record['worksite_postal_code']
+            
+            if 'worksite_city' in job_record: 
+                worksite_city = job_record['worksite_city']
+            else:
+                non_empty_worksite = 0
+                
+            if 'worksite_county' in job_record: 
+                worksite_county = job_record['worksite_county']
+            else:
+                non_empty_worksite = 0
+
+            if 'worksite_state' in job_record: 
+                worksite_state = job_record['worksite_state']
+            else:
+                non_empty_worksite = 0
+
+            if 'worksite_postal_code' in job_record: 
+                worksite_postal_code = job_record['worksite_postal_code']
+            else:
+                non_empty_worksite = 0
+
+            if non_empty_worksite == 1:
+                worksite_postal_code_start = worksite_postal_code[0]
                 if not worksite_postal_code_start.isdigit():
-                    worksite_city = job_record['worksite_county']
-                    worksite_county = job_record['worksite_state']
-                    worksite_state = job_record['worksite_postal_code']
+                    # place them into the appropriate columns 
+                    worksite_city = worksite_county
+                    worksite_county = worksite_state
+                    worksite_state = worksite_postal_code
+                    job_record.pop('worksite_postal_code')
                     job_record['worksite_city'] = worksite_city
                     job_record['worksite_county'] = worksite_county
                     job_record['worksite_state'] = worksite_state
-                    job_record.pop('worksite_postal_code')
-             
-            # check to see if soc_code and soc_name are swapped
-            valid_soc = 1
-            if job_record['soc_name'] == None:
-                valid_soc = 0
-                job_record.pop('soc_name')
-            if job_record['soc_code'] == None:
-                valid_soc = 0
-                job_record.pop('soc_code')
-            if valid_soc == 1:   
-                soc_name_start = job_record['soc_name'][0]
+                    
+                # remove suffix from worksite_county
+                if worksite_county != None:
+                    worksite_county = worksite_county.replace(' COUNTY', '')
+                    job_record['worksite_county'] = worksite_county
+                    
+                # county and state should not be equal to each other
+                if worksite_county == worksite_state:
+                    worksite_county = None 
+                    job_record.pop('worksite_county')
+                elif worksite_county.isdigit():
+                    worksite_county = None 
+                    job_record.pop('worksite_county')
+               
+            # validate soc_code and soc_name 
+            non_empty_soc = 1
+            
+            if 'soc_code' in job_record:
+                soc_code = job_record['soc_code']
+            else:
+                non_empty_soc = 0
+            
+            if 'soc_name' in job_record:
+                soc_name = job_record['soc_name']
+            else:
+                non_empty_soc = 0
+
+            if non_empty_soc == 1:   
+                soc_name_start = soc_name[0]
                 if soc_name_start.isdigit():
+                    # soc_code and soc_name are swapped
                     soc_code = job_record['soc_name']
                     soc_name = job_record['soc_code']
-                    soc_code = soc_code.replace('.00', '')
-                    job_record['soc_code'] = soc_code
                     job_record['soc_name'] = soc_name
-            
+                # remove invalid suffix from soc_code    
+                soc_code = soc_code.replace('.00', '')
+                job_record['soc_code'] = soc_code
+                
             # check to see if pw_unit_of_pay is really the prevailing_wage
-            if job_record['pw_unit_of_pay'] == None:
-                job_record.pop('pw_unit_of_pay')
-            else:
-                pw_unit_of_pay_start = job_record['pw_unit_of_pay'][0]
+            if 'pw_unit_of_pay' in job_record:
+                pw_unit_of_pay = job_record['pw_unit_of_pay']
+                pw_unit_of_pay_start = pw_unit_of_pay[0]
                 if pw_unit_of_pay_start.isdigit():
                     prevailing_wage = job_record['pw_unit_of_pay']
                     job_record.pop('pw_unit_of_pay')
                     job_record['prevailing_wage'] = prevailing_wage
                 
-            if job_record['wage_rate_of_pay_from'] == None:
-                job_record.pop('wage_rate_of_pay_from')
-            if job_record['wage_rate_of_pay_to'] == None:
-                job_record.pop('wage_rate_of_pay_to')     
-            if job_record['full_time_position'] == None:
-                job_record.pop('full_time_position')
-            if job_record['pw_source_year'] == None:
-                job_record.pop('pw_source_year')
-            if job_record['pw_source_other'] == None:
-                job_record.pop('pw_source_other')
-                
+            if 'wage_rate_of_pay_to' in job_record:
+                wage_rate_of_pay_to = job_record['wage_rate_of_pay_to']
+                if wage_rate_of_pay_to == 0.0:
+                    job_record.pop('wage_rate_of_pay_to')     
+            
             job_record['employer_id'] = employer_id 
-            job_record.pop('employer_name')
-            job_record.pop('employer_city')
-            job_record.pop('employer_state')
+            
+            # remove employer_name, employer_city, employer_state from the job record
+            if 'employer_name' in job_record:
+                job_record.pop('employer_name')
+            
+            if 'employer_city' in job_record:
+                job_record.pop('employer_city')   
+
+            if 'employer_state' in job_record:
+                job_record.pop('employer_state')
             
             job_results.append(job_record)
                         
@@ -170,9 +203,15 @@ opts = beam.pipeline.PipelineOptions(flags=[], **options)
 
 with beam.Pipeline('DirectRunner', options=opts) as p:
     
-    job_query_results = p | 'Read from BigQuery Job' >> beam.io.Read(beam.io.BigQuerySource(query='SELECT * FROM h1b_split.Job_Temp where employer_name like \'%PROMATRIX%\''))
-    emp_query_results = p | 'Read from BigQuery Employer' >> beam.io.Read(beam.io.BigQuerySource(query='SELECT employer_id, employer_name, employer_city FROM ' \
-                                                                                                       'h1b_split.Employer where employer_name like \'%PROMATRIX%\''))
+    job_query_str = 'SELECT *, EXTRACT(YEAR FROM employment_start_date) AS employment_start_year ' \
+                    'FROM `h1b_split.Job_Temp` ORDER BY employer_name LIMIT 100'
+    
+    emp_query_str = 'SELECT employer_id, employer_name, employer_city FROM ' \
+                    '`h1b_split.Employer` ORDER BY employer_name LIMIT 100'
+    
+    job_query_results = p | 'Read from BigQuery Job' >> beam.io.Read(beam.io.BigQuerySource(query=job_query_str, use_standard_sql=True))
+    emp_query_results = p | 'Read from BigQuery Employer' >> beam.io.Read(beam.io.BigQuerySource(query=emp_query_str, use_standard_sql=True))
+    
     # apply ParDo to the Job records 
     job_tuple_pcoll = job_query_results | 'Transform Job Record' >> beam.ParDo(TransformJobRecord())
     emp_tuple_pcoll = emp_query_results | 'Transform Employer Record' >> beam.ParDo(TransformEmployerRecord())
@@ -188,7 +227,7 @@ with beam.Pipeline('DirectRunner', options=opts) as p:
     job_bq_pcoll | 'Write to File 4' >> WriteToText('output_bq_record.txt')
     
     qualified_table_name = PROJECT_ID + ':h1b_split.Job'
-    table_schema = 'job_id:STRING,employer_id:STRING,employment_start_date:DATE,employment_end_date:DATE,job_title:STRING,'\
+    table_schema = 'job_id:STRING,employer_id:STRING,employment_start_year:INTEGER,employment_start_date:DATE,employment_end_date:DATE,job_title:STRING,'\
                     'wage_rate_of_pay_from:FLOAT,wage_rate_of_pay_to:FLOAT,wage_unit_of_pay:STRING,worksite_city:STRING,worksite_county:STRING,'\
                     'worksite_state:STRING,worksite_postal_code:STRING,soc_code:STRING,soc_name:STRING,total_workers:INTEGER,full_time_position:BOOLEAN,'\
                     'prevailing_wage:FLOAT,pw_unit_of_pay:STRING,pw_source:STRING,pw_source_year:STRING,pw_source_other:STRING'
